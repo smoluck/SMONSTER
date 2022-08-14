ab94aec36beaac97412cbea66fe5d83042bb7e63
smoluck <smoluck@users.noreply.github.com>
2022-08-14 15:34:29 +0900

    SMO GC - Replace by Instance
    SMO GC - Replace by Instance
    (From selected targets, create a copy or an instance of the last selected Mesh, with or without a Guide Mesh.)

--- KITS/SMONSTER/lxserv/SMO_GC_ReplaceTargetByInstance_Cmd.py
+++ KITS/SMONSTER/lxserv/SMO_GC_ReplaceTargetByInstance_Cmd.py
@@ -4,8 +4,8 @@
 # Version:      1.0
 #
 # Purpose:      This script is designed to:
-#               Delete all 3DSMAX related Channels created
-#               at FBX Export in the current scene.
+#               From selected targets, create a copy or an instance of the last selected Mesh, without or with a Guide Mesh creation.
+#
 #
 # Author:       Franck ELISABETH (based on William Vaughan Script)
 # Website:      http://www.smoluck.com
@@ -15,7 +15,7 @@
 #---------------------------------------------
 
 
-import lx, lxu, modo
+import lx, lxu, modo, sys
 
 Cmd_Name = "smo.GC.ReplaceTargetByInstance"
 # smo.GC.ReplaceTargetByInstance 1 # copy in instance mode
@@ -24,7 +24,10 @@ class SMO_GC_ReplaceTargetByInstance_Cmd(lxu.command.BasicCommand):
     def __init__(self):
         lxu.command.BasicCommand.__init__(self)
         self.dyna_Add("Item Type", lx.symbol.sTYPE_INTEGER)
-        self.basic_SetFlags (0, lx.symbol.fCMDARG_OPTIONAL)             # here the (0) define the argument index.
+        self.basic_SetFlags(0, lx.symbol.fCMDARG_OPTIONAL)             # here the (0) define the argument index.
+        self.dyna_Add("Create a Guide Mesh from Target", lx.symbol.sTYPE_INTEGER)
+        self.basic_SetFlags(1, lx.symbol.fCMDARG_OPTIONAL)
+
     def cmd_Flags(self):
         return lx.symbol.fCMD_MODEL | lx.symbol.fCMD_UNDO
     
@@ -35,10 +38,10 @@ class SMO_GC_ReplaceTargetByInstance_Cmd(lxu.command.BasicCommand):
         return 'SMO GC - Replace By Instance'
     
     def cmd_Desc (self):
-        return 'Select the target then the source Mesh Item.'
+        return 'From selected targets, create a copy or an instance of the last selected Mesh, without or with a Guide Mesh creation.'
     
     def cmd_Tooltip (self):
-        return 'Select the target then the source Mesh Item.'
+        return 'From selected targets, create a copy or an instance of the last selected Mesh, without or with a Guide Mesh creation.'
     
     def cmd_Help (self):
         return 'https://twitter.com/sm0luck'
@@ -50,83 +53,104 @@ class SMO_GC_ReplaceTargetByInstance_Cmd(lxu.command.BasicCommand):
         return True
     
     def basic_Execute(self, msg, flags):
-        ItemType = self.dyna_Int (0)
-        if ItemType == 0 :
+        scene = modo.Scene()
+        item_type = self.dyna_Int(0)
+        guide_mode = self.dyna_Int(1)
+
+        if item_type == 0:
             lx.out('Regular Clone Mode')
-        elif ItemType == 1 :
+        elif item_type == 1:
             lx.out('Instance Clone Mode')
-        
-        
+
         SelectedItemsCount = len(lx.evalN("query sceneservice selection ? locator"))
-        
+
         if SelectedItemsCount < 2:
             lx.eval('dialog.setup info')
             lx.eval('dialog.title {Replace Target By Instance:}')
             lx.eval('dialog.msg {"You must have at least 2 items selected to run this script.}')
             lx.eval('+dialog.open')
             sys.exit()
-        
-        SelectedItemsCount = (SelectedItemsCount -1)
-        lx.out('Selected Target Count:',SelectedItemsCount)
-        
-        selMeshes = lx.eval('query sceneservice selection ? mesh')
-        lx.out('selMeshes',selMeshes)
-        
-        sourceMesh = selMeshes[-1]
-        lx.out(sourceMesh)
-        
-        meshnum = 0
-        
-        #deselect items
-        lx.eval('select.drop item')
-        
-        for r in range(SelectedItemsCount):
-            lx.eval('select.item {%s} set' %selMeshes[meshnum])
-            m = lx.eval('query sceneservice selection ? mesh')
-            
-            #Get item Position, Scale and Rotation
-            mPos = lx.eval('query sceneservice item.Pos ? %s' % m)      #Queries the current item XYZ position
-            mRot = lx.eval('query sceneservice item.Rot ? %s' % m)      #Queries the current item Rotation XYZ value
-            mScl = lx.eval('query sceneservice item.Scale ? %s' % m)    #Queries the current item Scale XYZ value
-            lx.out(mPos)
-            lx.out(mRot)
-            lx.out(mScl)
-            
-            mRotX = ((mRot[0] *180) /3.14)
-            mRotY = ((mRot[1] *180) /3.14)
-            mRotZ = ((mRot[2] *180) /3.14)
-            
-            #delete
-            lx.eval('!delete')
-            
-            lx.eval('select.item {%s} set' %sourceMesh)
-            if ItemType == 0 :
-                lx.eval('item.duplicate')
-            elif ItemType == 1 :
-                lx.eval('item.duplicate true locator false true')
-            
-            #Move the new item
-            lx.eval('transform.channel pos.X %s'%mPos[0])
-            lx.eval('transform.channel pos.Y %s'%mPos[1])
-            lx.eval('transform.channel pos.Z %s'%mPos[2])
-            lx.eval('tool.doApply')
-            
-            #Rotate the new item
-            lx.eval('transform.channel rot.X %s'%mRotX)
-            lx.eval('transform.channel rot.Y %s'%mRotY)
-            lx.eval('transform.channel rot.Z %s'%mRotZ)
-            
-            
-            #Scale the new item
-            lx.eval('transform.channel scl.X %s'%mScl[0])
-            lx.eval('transform.channel scl.Y %s'%mScl[1])
-            lx.eval('transform.channel scl.Z %s'%mScl[2])
-            
-            
-            #deselect items
-            lx.eval('select.drop item')
-            
-            meshnum +=1
+
+        if SelectedItemsCount >= 2:
+            SelectedItemsCount = (SelectedItemsCount - 1)
+            lx.out('Selected Target Count:', SelectedItemsCount)
+
+            selMeshes = lx.eval('query sceneservice selection ? mesh')
+            lx.out('selMeshes', selMeshes)
+            TargetMeshes = scene.selected
+
+            sourceMesh = selMeshes[-1]
+            lx.out(sourceMesh)
+
+            meshnum = 0
+
+            # deselect items
+            lx.eval('smo.GC.DeselectAll')
+
+            if guide_mode == 1:
+                lx.eval("layer.new")
+                GuideMesh = scene.selected
+                lx.eval("item.name Guide mesh")
+                lx.eval("smo.CB.ItemColor 8 1")
+                lx.eval('smo.GC.DeselectAll')
+                scene.select(TargetMeshes)
+                lx.eval("select.type polygon")
+                lx.eval("select.all")
+                lx.eval("copy")
+                scene.select(GuideMesh)
+                lx.eval("paste")
+                lx.eval("select.drop polygon")
+                lx.eval("select.type item")
+                lx.eval('smo.GC.DeselectAll')
+
+            for r in range(SelectedItemsCount):
+                lx.eval('select.item {%s} set' % selMeshes[meshnum])
+                m = lx.eval('query sceneservice selection ? mesh')
+                # print(m)
+
+                # Get item Position, Scale and Rotation
+                m_pos = lx.eval('query sceneservice item.Pos ? %s' % m)  # Queries the current item XYZ position
+                m_rot = lx.eval('query sceneservice item.Rot ? %s' % m)  # Queries the current item Rotation XYZ value
+                m_scl = lx.eval('query sceneservice item.Scale ? %s' % m)  # Queries the current item Scale XYZ value
+                lx.out(m_pos)
+                lx.out(m_rot)
+                lx.out(m_scl)
+
+                m_rot_x = ((m_rot[0] * 180) / 3.14)
+                m_rot_y = ((m_rot[1] * 180) / 3.14)
+                m_rot_z = ((m_rot[2] * 180) / 3.14)
+                # print(m_rot_x)
+                # print(m_rot_y)
+                # print(m_rot_z)
+
+                # delete
+                lx.eval('!delete')
+
+                lx.eval('select.item {%s} set' % sourceMesh)
+                if item_type == 0:
+                    lx.eval('item.duplicate')
+                elif item_type == 1:
+                    lx.eval('item.duplicate true locator false true')
+
+                # Move the new item
+                lx.eval('transform.channel pos.X %s' % m_pos[0])
+                lx.eval('transform.channel pos.Y %s' % m_pos[1])
+                lx.eval('transform.channel pos.Z %s' % m_pos[2])
+
+                # Rotate the new item
+                lx.eval('transform.channel rot.X %s' % m_rot_x)
+                lx.eval('transform.channel rot.Y %s' % m_rot_y)
+                lx.eval('transform.channel rot.Z %s' % m_rot_z)
+
+                # Scale the new item
+                lx.eval('transform.channel scl.X %s' % m_scl[0])
+                lx.eval('transform.channel scl.Y %s' % m_scl[1])
+                lx.eval('transform.channel scl.Z %s' % m_scl[2])
+
+                # deselect items
+                lx.eval('select.drop item')
+
+                meshnum += 1
         
     
 lx.bless(SMO_GC_ReplaceTargetByInstance_Cmd, Cmd_Name)
